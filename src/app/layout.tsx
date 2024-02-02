1b6e2470d490895a75cf67cee47f39ca8876e941
Ishan Trivedi <astrocoder8046@gmail.com>
2024-02-03 02:43:37 +0530

    [ADD] Custom extentions to tailwind.config.ts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Ishan Trivedi",
   description: "Ishan Trivedi's Portfolio Website",
+  icons: "/logo.svg",
 };
 
 export default function RootLayout({
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,44 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-conic":
+          "conic-gradient( from 180deg at 50% 50%, #0aefff -69.37deg, #0f83ff 31.88deg, #b056e7 120deg, #ff9966 204.37deg, #0aefff 290.63deg, #0f83ff 391.87deg)",
+      },
+      screens: {
+        xxxxs: "360px",
+        xxxs: "375px",
+        xxs: "390px",
+        xs: "412px",
+        sm: "810px",
+        md: "1280px",
+        lg: "1366px",
+        lg2: "1440px",
+        lg3: "1536px",
+        lg4: "1600px",
+        lg5: "1920px",
+        lg6: "2560px",
+      },
+      colors: {
+        gray: {
+          200: "#f9f8f8",
+        },
+        red: {
+          100: "rgba(248, 0, 0, 0.7)",
+        },
+        orange: {
+          100: "#FBA500",
+        },
+        purple: {
+          900: "#2D043C",
+        },
+        green: {
+          100: "#8FD004",
+          900: "#004710",
+        },
+      },
+    },
   },
   plugins: [],
 };
