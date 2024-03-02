0b473e9fa1ec4abce645c732aaac9b3ece58ab96
Ishan Trivedi <astrocoder8046@gmail.com>
2024-03-02 16:57:49 +0530

    [Update]: Header menu order

--- src/app/components/header/constant.ts
+++ src/app/components/header/constant.ts
@@ -3,6 +3,6 @@ import { NavigationItem } from "@/shared-resources/contracts/globalTypes";
 export const NAVIGATION_ITEMS: Array<NavigationItem> = [
   { href: "#work", title: "Work" },
   { href: "#projects", title: "Projects" },
-  { href: "#benefits", title: "Benefits" },
   { href: "#expertise", title: "Expertise" },
+  { href: "#benefits", title: "Benefits" },
 ];
