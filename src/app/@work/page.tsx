34bf7f7716370123fa5183ae4983edfce889d8ed
Ishan Trivedi <astrocoder8046@gmail.com>
2024-04-07 17:36:54 +0530

    feat: :sparkles: added responsiveness to work page

--- src/app/@work/page.tsx
+++ src/app/@work/page.tsx
@@ -29,17 +29,17 @@ const WorkPage = () => {
         <AnimatedCard
           observedElementId="devslane__element"
           containerId="devslane__container"
-          wrapClassnames="w-full sm:w-[80%]"
+          wrapClassnames="w-full md:w-[80%]"
         >
-          <div className="w-full h-[18rem] sm:h-[32rem] justify-between flex flex-row rounded-2xl bg-gradient-to-br from-red-100 to-orange-100 sm:px-8 px-6 sm:py-12 py-8  text-white pointer-events-none">
-            <div className="h-full w-1/3 font-semibold text-lg text-right sm:block hidden">
+          <div className="w-full h-[18rem] sm:h-[32rem] justify-between flex flex-row rounded-2xl bg-gradient-to-br from-red-100 to-orange-100 sm:px-8 px-6 sm:py-12 py-6  text-white pointer-events-none">
+            <div className="h-full w-1/3 font-semibold text-lg text-right md:block hidden">
               2021 - Present
             </div>
-            <div className="sm:h-full sm:w-3/5 w-full flex flex-col items-start">
+            <div className="md:h-full md:w-3/5 w-full flex flex-col items-start">
               <div className="font-semibold sm:text-2xl text-base">
                 Software Developer - Devslane
               </div>
-              <div className="w-full font-medium text-sm mt-1 text-left sm:hidden">
+              <div className="w-full font-medium text-sm sm:text-lg mt-1 text-left md:hidden">
                 2021 - Present
               </div>
               <div className="font-medium sm:text-lg text-sm mt-4">
@@ -49,11 +49,11 @@ const WorkPage = () => {
                 collaboration, knowledge shares, and spearheading the
                 development of internal tools.
               </div>
-              <div className="flex-wrap gap-4 hidden sm:flex justify-start mt-8">
+              <div className="flex-wrap sm:gap-4 gap-3 hidden ipad-mini:flex w-full justify-start sm:mt-8 ipad-mini:mt-4">
                 {DEVSLANE_SKILLS.map((skill) => (
                   <div
                     key={skill}
-                    className="font-medium px-4 py-2 border-white border-[2px] rounded-2xl flex justify-center items-center"
+                    className="font-medium sm:text-base text-sm sm:px-4 sm:py-2 p-2 border-white sm:border-[2px] border rounded-2xl flex justify-center items-center"
                   >
                     {skill}
                   </div>
@@ -65,17 +65,17 @@ const WorkPage = () => {
         <AnimatedCard
           observedElementId="geeksforgeeks__element"
           containerId="geeksforgeeks__container"
-          wrapClassnames="w-full sm:w-[80%]"
+          wrapClassnames="w-full md:w-[80%]"
         >
-          <div className="w-full h-[18rem] sm:h-[32rem] flex justify-between text-white rounded-2xl bg-gradient-to-br from-green-100 sm:px-8 px-6 sm:py-12 py-8 to-green-900 p-4 pointer-events-none">
-            <div className="h-full w-1/3 font-semibold text-lg text-right sm:block hidden">
+          <div className="w-full h-[18rem] sm:h-[32rem] flex justify-between text-white rounded-2xl bg-gradient-to-br from-green-100 sm:px-8 px-6 sm:py-12 py-6 to-green-900 p-4 pointer-events-none">
+            <div className="h-full w-1/3 font-semibold text-lg text-right md:block hidden">
               Dec 2019 - Apr 2020
             </div>
-            <div className="sm:h-full sm:w-3/5 w-full flex flex-col items-start">
+            <div className="md:h-full md:w-3/5 w-full flex flex-col items-start">
               <div className="font-semibold sm:text-2xl text-base">
                 TCW Intern - GeeksForGeeks
               </div>
-              <div className="w-full font-medium text-sm mt-1 text-left sm:hidden">
+              <div className="w-full font-medium text-sm mt-1 sm:text-lg text-left md:hidden">
                 Dec 2019 - Apr 2020
               </div>
               <div className="font-medium sm:text-lg text-sm mt-4">
@@ -84,11 +84,11 @@ const WorkPage = () => {
                 certainly enhanced my understanding of data structure and
                 algortihms and improved my problem-solving skills.
               </div>
-              <div className="sm:flex hidden flex-wrap gap-4 justify-start mt-8">
+              <div className="flex-wrap sm:gap-4 gap-3 hidden ipad-mini:flex w-full justify-start sm:mt-8 ipad-mini:mt-4">
                 {GEEKS_FOR_GEEKS_SKILLS.map((skill) => (
                   <div
                     key={skill}
-                    className="font-medium px-4 py-2 border-white border-[2px] rounded-2xl flex justify-center items-center"
+                    className="font-medium sm:text-base text-sm sm:px-4 sm:py-2 p-2 border-white sm:border-[2px] border rounded-2xl flex justify-center items-center"
                   >
                     {skill}
                   </div>
